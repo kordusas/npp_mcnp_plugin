336d1c3e8cf3e0cdbdcd41dc48cea91ad61465fe
kordusas <bmarcinkevicius@gmail.com>
2024-07-03 19:03:27 +0200

    created function to format the notifier message from the results. placing here decouples the code. checks if it is list str or object and prints

--- _common/general_utils.py
+++ _common/general_utils.py
@@ -1,7 +1,22 @@
 
 from Npp import console
 
-
+# Add the format_notifier_message function
+def format_notifier_message(items_to_show):
+    """
+    Formats message for the notifier into a string.
+    Args:
+        items_to_show (str | list | object): The items to format into a message string.
+    Returns:
+        str: The formatted message.
+    """
+    if isinstance(items_to_show, str):
+        return items_to_show
+    elif isinstance(items_to_show, list):
+        return '\n'.join([str(item) for item in items_to_show])
+    else:  # Assuming it's an object instance
+        return str(items_to_show)
+    
 def log_debug(debug, message):
     """
     Log a debug message to the console.
