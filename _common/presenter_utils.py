db1f8fa0fddeb5e344b8969436c6ebf8fde9bc19
kordusas <bmarcinkevicius@gmail.com>
2024-07-11 23:01:50 +0200

    added method which checks whether it is cell definition selected. if so prints surface information. now doesnt interpret density as sutface digit.

--- _common/presenter_utils.py
+++ _common/presenter_utils.py
@@ -37,10 +37,6 @@ class AbstractBlockSelectionPresenter(object):
         # To be implemented by subclasses
         pass
 
-
-
-
-
 class CellBlockPresenter(AbstractBlockSelectionPresenter):
     def __init__(self, view_of_current_line, mcnp_input, notifier, debug=True):
         super(CellBlockPresenter, self).__init__(view_of_current_line, mcnp_input, notifier, debug)   
@@ -140,8 +136,27 @@ class CellBlockPresenter(AbstractBlockSelectionPresenter):
         # find the selected surfaces in the mcnp input
         selected_surfaces = [ self.mcnp_input.get_surface(validate_return_id_as_int(surface_id)) for surface_id in selected_surface_ids  ] 
         return {"type": "surface_id", "value": format_notifier_message(selected_surfaces)}
-    def is_surface_selected(self):
-        # placeholder for now
+    
+    def is_cell_definition_selected(self):
+        log_debug(self.debug, "Called method is_cell_definition_selected\n")
+        # get second entry in the line which is material id
+        material_id = self.view_of_selected_line.current_line_list[1]
+
+        
+        # if material is not void then cell definition starts after third entry(index is 0 based)
+        index_of_token = 2
+        # if material is void then then cell definition starts after second entry(1ist index is 0 based
+        if material_id.isdigit() and material_id == 0:
+            index_of_token = 1 
+        
+            
+        
+        cell_definition_start = self.view_of_selected_line.find_space_separated_token_end_positions(index_of_token)
+        log_debug(self.debug, "Cell definition start: {}\n".format(cell_definition_start))
+        log_debug(self.debug, "Selection end: {}\n".format(self.view_of_selected_line.selection_end))
+        if self.view_of_selected_line.selection_start <= cell_definition_start:
+            log_debug(self.debug, "Cell definition is not selected\n")
+            return False
         return True
     
     def analyze_selection(self):
@@ -162,10 +177,10 @@ class CellBlockPresenter(AbstractBlockSelectionPresenter):
 
         elif self.is_material_id_selected():
             return self._handle_material_id_selected()
-        elif self.is_surface_selected():
+        elif self.is_cell_definition_selected():
             return self._handle_surfaces_selected()
 
-        return False
+        return None
 
 
 class PhysicsBlockPresenter(AbstractBlockSelectionPresenter):
