fe9e97c6371aa60a7e3504e9705ff8490b0b2ad2
kordusas <bmarcinkevicius@gmail.com>
2024-07-18 13:43:40 +0200

    fisrt comit of AutocompleteNotification class

--- _common/notification_utils.py
+++ _common/notification_utils.py
@@ -48,3 +48,24 @@ class SelectionNotification():
             self._popup_notification(analysis_result["value"])
 
             
+class AutocompleteNotification():
+    """
+    This class is used to pop notifications when a block of text is selected.
+    Implements Singleton pattern to ensure only one instance is used throughout the application.
+    """
+    _instance = None
+
+    def __new__(cls):
+        if cls._instance is None:
+            cls._instance = super(AutocompleteNotification, cls).__new__(cls)
+        return cls._instance
+    def __init__(self):
+        pass
+    def _popup_notification(self, message):
+        editor.autoCShow(0, message)
+     
+    def notify(self, analysis_result):
+        action_type = analysis_result.get("type")
+        value = analysis_result.get("value")
+        if value:
+            self._popup_notification(analysis_result["value"])
