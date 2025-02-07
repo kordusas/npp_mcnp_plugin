4a9b1abc3cf445a664ec42892bbe15296e018937
kordusas <bmarcinkevicius@gmail.com>
2025-02-07 16:00:28 +0100

    improved file parser using regex, should be faster. dropped comments after $ and in between card definitions

--- utils/file_parser.py
+++ utils/file_parser.py
@@ -7,140 +7,18 @@ from npp_mcnp_plugin.models.mcnp_cell_factory import CellFactory
 from npp_mcnp_plugin.models.error import  ErrorModel
 from npp_mcnp_plugin.utils.string_utils import is_comment_line, is_match_at_start
 
-
+_REPEAT_RE = re.compile(r'(\d+)\s+(\d+)[rR]')
 class FileParser(object):
     def __init__(self, filename, error_collection):
         self.filename = filename
 
         self.error_collection = error_collection
+        self.content = None
         self.lines = None
-        self.message_block = None
-        self.has_header = False
         self.block = {}
         self.block_locations = {}
         self.title  = ""
         self.logger = logging.getLogger(self.__class__.__name__)
-         
-    def set_header_flag(self):
-        # Determine if we have a header block
-        self.has_header = self.lines[0].startswith("message")
-
-    def set_block_locations(self):
-        """
-        Finds and sets the start and end lines of each block of data in the file.
-
-        This method identifies the indices of empty lines in the file, which are used to determine the boundaries of different blocks of data (cells, surfaces, and physics). It then sets the start and end lines for each block in the `block_locations` attribute. If a header is present, it adjusts the indices accordingly.
-
-        Returns:
-            None
-        """
-        self.logger.info("Called method set_block_locations...\n")
-
-        block_start_indices = []
-        offset = 1  # Offset to account for the title line and empty lines
-
-        for i, line in enumerate(self.lines):
-            if line.strip() == "":
-                block_start_indices.append(i)
-
-        if self.has_header:
-            self.parse_header(block_start_indices[0])
-            block_start_indices[0] += offset  # adding offset as this is an empty line
-        else:
-            block_start_indices.insert(0, 0)
-
-        self.block_locations['cells'] = {'start': block_start_indices[0] + offset, 'end': block_start_indices[1]}
-        self.block_locations['surfaces'] = {'start': block_start_indices[1] + offset, 'end': block_start_indices[2]}
-        self.block_locations['physics'] = {'start': block_start_indices[2] + offset, 'end': block_start_indices[3] + offset if len(block_start_indices) > 3 else len(self.lines)}
-                
-        return 
-    def parse_header(self, line_no):
-        self.message_block = self.lines[:line_no]
-
-    def parse_title(self):
-        self.title = self.lines[self.block_locations['cells']['start']-1]
-    def parse_blocks(self):
-        for key in ['cells','surfaces', 'physics']:
-            self.block[key] =self.format_blocks(self.lines[self.block_locations[key]['start']:self.block_locations[key]['end']])
-  
-
-    def parse(self):
-        """
-        Parses the different blocks of data in the file.
-        I merge the continuation lines for better interpretation of the data.
-
-        """
-        self.parse_blocks()
-        self.parse_title()
-
-        self.logger.info("Cells block: %d\nSurfaces block: %d\nPhysics block: %d\n",
-            len(self.block["cells"]), len(self.block["surfaces"]), len(self.block["physics"])
-        )
-
-    def format_blocks(self, block):
-        """
-        removes leading spaces if less than 4 spaces 
-
-        Merges continuation lines in the file.
-        If the line starts with 4 spaces, it is a continuation line.
-        If the line has a comment after a '$', it is appended to comment_line
-        the comment_line is then appended to the previous line.
-
-        If accidenally the line is empty we skip it.
-
-        Adds space in front of the "(" to make sure that parsing always works; special case where after density cell can start with "(" without space  
-                  """
-        merged_block = []
-        comment = ""
-        for  line in block:
-            line = line.replace('(', ' (')
-            line = line.rstrip('\n')
-            if line.strip() == "":
-                continue
-            # formatting the line and comment
-            line, comment = self.split_comment_from_line(line, comment)
-           
-            
-            # if lines starts with less than 4 spaces then remove them
-            line = self._remove_leading_spaces(line)
-
-            # merging the continuation lines
-            if not merged_block:
-                self._add_new_line(merged_block, line, comment)
-                comment = ""
-            elif line.startswith("    ") or merged_block[-1].endswith('&'):
-                merged_block[-1] = merged_block[-1].rstrip('&') + line
-            elif line.startswith("c"):
-                comment += " " + line[1:]
-            else:
-                self._add_new_line(merged_block, line, comment)
-                comment = ""
-
-        return merged_block
-    
-    def _remove_leading_spaces(self, line):
-        """
-        Removes leading spaces from a line if there are fewer than 4 spaces.
-
-        This method is used to handle indentation in MCNP input files. Lines with
-        fewer than 4 leading spaces are treated as new entries, while lines with
-        4 or more spaces are considered continuation lines.
-
-        Args:
-            line (str): The input line to process.
-
-        Returns:
-            str: The line with leading spaces removed if there were fewer than 4,
-                 otherwise the original line.
-        """
-        if len(line) - len(line.lstrip(' ')) < 4:
-            return line.lstrip(' ')
-        return line
-    def _add_new_line(self, merged_block, line, comment):
-        merged_block.append(line)
-        if comment:
-            merged_block.insert(-2, "c " + comment.replace("--", "").replace("==", "").replace("||", "").strip())    
-
     def _parse_block(self, block, regex_pattern, create_instance_func):
         """
         Generic method to parse a block of lines based on a regex pattern and create instances using a provided function.
@@ -227,28 +105,77 @@ class FileParser(object):
                 information_dict['nps'] = int(float(line.split()[1]))
         return information_dict
 
-    def split_comment_from_line(self, line, comment=""):
-        """
-        Splits the comment from the line and returns the line and comment separately.
-        if comment doesnt exist does nothing
-        Args:
-            line (str): The line to split.
-
-        Returns:
-            line (str): The line without the comment.
-            comment (str): The comment.
-        """
-        comment_new = ""        
-        if "$" in line:
-            line, comment_new = line.split("$", 1)
-
-        # stripping the line  from right side to remove any trailing spaces
-        return line.rstrip(" "), comment + comment_new
-
     def analyse_file(self):
-        self.set_header_flag()
-        self.set_block_locations()
-        self.parse()
+        self.format_sanitize_content()
+        self.split_blocks()
+        self.format_sanitize_blocks()
+
+
+    @staticmethod
+    def count_block_start_locations( sections):
+        block_start_indices = []
+        line_number = 0  # Initialize line_number
+        for section in sections:
+        # Count newlines up to this section
+            line_number += section.count('\n')
+            line_number += 2
+            block_start_indices.append((line_number, section.strip()))
+            
+        return block_start_indices
+    
+    def split_blocks(self):
+
+        sections = re.split(r'\n[ \t]*\n', self.content)
+        block_start_indices = self.count_block_start_locations(sections)
+        # count number of new lines in each block and append to block_start_indices list 
+        if sections[0].startswith("\s+message"):
+            self.block["message"] = sections[0]
+            sections = sections[1:]
+            block_start_indices = block_start_indices[1:]
+        else:
+            block_start_indices.insert(0, (0, ''))
+            
+        self.block["cells"] = sections[0]   
+        self.block["surfaces"] = sections[1]
+        self.block["physics"] = sections[2]
+
+        self.block_locations['cells'] = {'start': block_start_indices[0][0] , 'end': block_start_indices[1][0] }
+        self.block_locations['surfaces'] = {'start': block_start_indices[1][0] , 'end': block_start_indices[2][0] }
+        self.block_locations['physics'] = {'start': block_start_indices[2][0] , 'end': block_start_indices[3][0] if len(block_start_indices) > 3 else len(self.content.splitlines())}
+        # clearing memory
+        self.content = []
+        
+                
+    def format_sanitize_blocks(self):
+        for key in self.block.keys():
+            # Turn continuation lines into single line
+            self.block[key] = re.sub('&.*\n', ' ', self.block[key])
+            self.block[key] = re.sub('\n {5}', ' ', self.block[key])
+
+            
+            # Remove all leading spaces after newlines
+            self.block[key] = re.sub(r'\n +', '\n', self.block[key])         
+
+            # Expand repeated numbers
+            m = _REPEAT_RE.search(self.block[key])
+            while m is not None:
+                self.block[key] = _REPEAT_RE.sub(' '.join((int(m.group(2)) + 1)*[m.group(1)]),
+                                        self.block[key], 1)
+                m = _REPEAT_RE.search(self.block[key])    
+                
+            # Split into lines and merge comments/continuations
+            
+            self.logger.debug("sanitized %s block content: %s", key, self.block[key])
+            self.block[key] = self.block[key].splitlines()
+
+    def format_sanitize_content(self):
+        self.content = self.content.lower() # make everything lowercase
+        self.content = re.sub(r'\(', ' (', self.content) # adding space before the ( (handling some edge case like this cell: 1 10 -2.0(3 4 5) 6 7 8 9)
+
+        # removing comments which are in the middle of the cards
+        self.content = re.sub('^[ \t]*?[cC].*?$\n {5}?', '', self.content, flags=re.MULTILINE)
+
+        self.content = re.sub(r'\$.*$', '', self.content, flags=re.MULTILINE) # remove inline comments   
         
 
     def read_new_file(self, file_path):
@@ -258,14 +185,14 @@ class FileParser(object):
 
     def read_file(self):
         """
-        Reads the file and stores the lines in the `lines` attribute in lower case.
+        Reads the file.
         """
         try:
             with open(self.filename, 'r') as file:
-                self.lines = [line.lower() for line in file.readlines()]
+                self.content = file.read()  # Assign the content to self.content
         except Exception as e:
-            self.logger.exception("Error reading file: {}\n".format(str(e))) 
-        
+            self.logger.exception("Error reading file: {}\n".format(str(e)))
+
     @classmethod
     def from_file(cls, file_path, error_collection):
         """
